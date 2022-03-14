b4438c1fcdb43a05b1ceb17b14af8bb34df08dcc
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-14 15:17:35 -0700

    rendering blog cards!

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function Blogs() {
+export default function Blogs({ id, author_id, created_at, title, subtitle, text }) {
+  // const [id, author_id, created_at, title, subtitle, text] = { ...props };
   return (
-    <div>Blogs</div>
+    <div>Blog
+      {id}
+      {author_id}
+      {created_at}
+      {title}
+      {subtitle}
+      {text}
+    </div>
   );
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,23 +1,27 @@
 import React, { useEffect } from 'react';
-import Blogs from '../components/Blogs/Blogs';
-// import { useState } from 'react';
+// import Blogs from '../components/Blogs/Blogs';
+import { useState } from 'react';
 import { fetchBlogs } from '../services/Blogs';
+import BlogCard from '../components/BlogCard/BlogCard';
 
 export default function Main() {
-  // const [blogs, setBlogs] = useState('');
+  const [blogs, setBlogs] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetchBlogs();
       console.log(resp);
-      // setBlogs(resp);
+      setBlogs(resp);
     };
     fetchData();
   }, []);
-
+  console.log('blogs', blogs);
   return (
     <div>
-      <Blogs />
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} title={blog.title} subtitle={blog.subtitle} text={blog.text} image={blog.image} />
+
+      ))}
     </div>
   );
 }
