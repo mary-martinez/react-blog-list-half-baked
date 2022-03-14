c500c405df01ca7bb3347c8268e9c46ce3b9a0c2
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-14 16:56:21 -0700

    fixing default of blogs useState to empty array

--- src/services/Blogs.js
+++ src/services/Blogs.js
@@ -2,6 +2,7 @@ import { checkError, client } from './client';
 
 
 export async function fetchBlogs() {
+  // const resp = await client.from('blogs').select('*, author_id(*)');
   const resp = await client.from('blogs').select('*');
   return (checkError(resp));
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -5,21 +5,21 @@ import { fetchBlogs } from '../services/Blogs';
 import BlogCard from '../components/BlogCard/BlogCard';
 
 export default function Main() {
-  const [blogs, setBlogs] = useState('');
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetchBlogs();
-      console.log(resp);
       setBlogs(resp);
     };
     fetchData();
   }, []);
-  console.log('blogs', blogs);
+  console.log(blogs);
   return (
     <div>
       {blogs.map((blog) => (
         <BlogCard key={blog.id} title={blog.title} subtitle={blog.subtitle} text={blog.text} image={blog.image} />
+        // <BlogCard key={blog.id} {...blog} />
 
       ))}
     </div>
