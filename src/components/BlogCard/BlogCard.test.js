1d52ad133f8322a8dc4a50c6ec835ccbea7e447f
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-14 17:31:22 -0700

    updating test with name object variable

--- src/components/BlogCard/BlogCard.test.js
+++ src/components/BlogCard/BlogCard.test.js
@@ -2,13 +2,14 @@ import { render, screen } from '@testing-library/react';
 import BlogCard from './BlogCard';
 
 test('should render the blog card with title, subtitle, author and text', () => {
+  const name = { 'name': 'author' };
   const container = render(
     <BlogCard
       title="Test title"
       subtitle="Test subtitle"
       text="lorem ipsum blah blah blah"
       image="https://www.fillmurray.com/300/200"
-      author_id={name = "Denise Volkman"}
+      author_id={name}
     />
   );
   const title = screen.getByText('Test title');
